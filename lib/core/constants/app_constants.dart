b138df54af08cadb1a4a4ea14e29adf1591c9228
Manucv-pe <manuelcardenas3103@gmail.com>
2026-08-12 10:15:53 -0500

    cambios

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -9,7 +9,7 @@
 class AppConstants {
   AppConstants._();
 
-  static const version = '1.0.5';
+  static const version = '1.0.6';
   static const nombreApp = 'GS1 CRM';
 
   // Separador para serialización de sesión en cadena
